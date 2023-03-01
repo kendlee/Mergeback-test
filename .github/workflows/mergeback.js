bb6acbf7836ddefcaab74dbabac5091517bf375b
Ken Darione Lee <ken.dlee@sitemate.com>
2023-03-01 16:43:07 +0800

    Add PR name

--- .github/workflows/mergeback.js
+++ .github/workflows/mergeback.js
@@ -1,4 +1,5 @@
 module.exports = async({context, ...data}) => {
   console.log(data)
   console.log(JSON.stringify(context, null, 2))
+  console.log('PR ref', context.payload.pull_request.head.ref)
 }
