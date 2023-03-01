985d395616ec767d9ff8a2478dc5fbc2670518e7
Ken Darione Lee <ken.dlee@sitemate.com>
2023-03-01 16:44:57 +0800

    add nullish coalescing or pull request

--- .github/workflows/mergeback.js
+++ .github/workflows/mergeback.js
@@ -1,5 +1,5 @@
 module.exports = async({context, ...data}) => {
   console.log(data)
   console.log(JSON.stringify(context, null, 2))
-  console.log('PR ref', context.payload.pull_request.head.ref)
+  console.log('PR ref', context.payload.pull_request?.head?.ref ?? 'none')
 }
