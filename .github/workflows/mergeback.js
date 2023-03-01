7d9911622506a7ab7268abedc79b6eda1083b79a
Ken Darione Lee <ken.dlee@sitemate.com>
2023-03-01 20:32:20 +0800

    query gh rest api

--- .github/workflows/mergeback.js
+++ .github/workflows/mergeback.js
@@ -1,5 +1,37 @@
-module.exports = async({context, ...data}) => {
-  console.log(data)
-  console.log(JSON.stringify(context, null, 2))
-  console.log('PR ref', context.payload.pull_request?.head?.ref ?? 'none')
+module.exports = async ({ github, context }) => {
+  console.log(data);
+  console.log(JSON.stringify(context, null, 2));
+  console.log("PR ref", context.payload.pull_request?.head?.ref ?? "none");
+
+  const branches = await github.rest.repos.listBranches({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    protected: true,
+    per_page: 100,
+  });
+
+  const branchesNames = branches.data
+    .map(({ name }) => name)
+    .filter((name) => name.startsWith("release"));
+  // .sort(sortBranchName);
+
+  const branchNameActionTrigger = context.ref.replace("refs/heads/", "");
+
+  console.log(branches);
+  console.log(branchNameActionTrigger);
+  console.log(branches.map(getNormalizedSemverVersion));
+};
+
+function getNormalizedSemverVersion(string) {
+  const number = string.split("/")[1];
+  let versions = number.split(".");
+
+  return versions.map((version) => version.padStart(2, "0")).join("");
+}
+
+function sortBranchName(branchName1, branchName2) {
+  const version1 = getNormalizedSemverVersion(branchName1);
+  const version2 = getNormalizedSemverVersion(branchName2);
+
+  return Number(versionNumber1) - Number(versionNumber2);
 }
