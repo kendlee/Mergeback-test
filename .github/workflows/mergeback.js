e64c5c7ce2d05f0ab59e19a3329c502a00931e4c
Ken Darione Lee <ken.dlee@sitemate.com>
2023-03-16 13:53:36 +0800

    Cleanup workflow

--- .github/workflows/mergeback.js
+++ .github/workflows/mergeback.js
@@ -1,6 +1,9 @@
 const RELEASE_PATTERN = "release/";
 const fs = require("fs");
 
+const falsyEntries = (a) => !!a;
+const removeLeadingTrailingSpaces = (a) => a.trim();
+
 module.exports = async ({ github, context }) => {
   const branchNameActionTrigger = context.ref.replace("refs/heads/", "");
   const mergedBranchName = context.payload.pull_request?.head?.ref;
@@ -20,12 +23,18 @@ module.exports = async ({ github, context }) => {
     `Detected merge from ${mergedBranchName} to ${branchNameActionTrigger}`
   );
 
-  const protectedBranches = await github.rest.repos.listBranches({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    protected: true,
-    per_page: 100,
-  });
+  const unmergedReleases = fs
+    .readFileSync("no-merged-releases.txt", "utf-8")
+    .split("\n")
+    .filter(falsyEntries)
+    .map(removeLeadingTrailingSpaces);
+
+  // const protectedBranches = await github.rest.repos.listBranches({
+  //   owner: context.repo.owner,
+  //   repo: context.repo.repo,
+  //   protected: true,
+  //   per_page: 100,
+  // });
 
   // const releaseBranches = protectedBranches.data
   //   .map(({ name }) => ({
@@ -46,15 +55,16 @@ module.exports = async ({ github, context }) => {
 
   // console.log(newerReleaseBranches);
   // console.log(otherReleaseBranches);
+  console.log("unmerged", unmergedReleases);
 
   // // TODO: change to map > 3rd param will be the targeted branches
-  // if (mergedBranchName) {
-  //   await createMergeBackPullRequest(
-  //     { github, context },
-  //     mergedBranchName,
-  //     otherReleaseBranches[0].name
-  //   );
-  // }
+  if (mergedBranchName) {
+    await createMergeBackPullRequest(
+      { github, context },
+      mergedBranchName,
+      unmergedReleases[0]
+    );
+  }
 };
 
 /**
@@ -113,40 +123,40 @@ module.exports = async ({ github, context }) => {
 //   return Number(version1) - Number(version2);
 // }
 
-// async function createMergeBackPullRequest(
-//   { github, context },
-//   sourceBranch,
-//   targetBranch
-// ) {
-//   const newBranchName = `merge-back-${context.sha.substring(
-//     0,
-//     7
-//   )}/${sourceBranch}-into-${targetBranch}`;
-//   console.log(`Creating mergeback: ${newBranchName}`);
+async function createMergeBackPullRequest(
+  { github, context },
+  sourceBranch,
+  targetBranch
+) {
+  const newBranchName = `merge-back-${context.sha.substring(
+    0,
+    7
+  )}/${sourceBranch}-into-${targetBranch}`;
+  console.log(`Creating mergeback: ${newBranchName}`);
 
-//   // Create new branch from base branch
-//   const newMergeBranch = await github.rest.git.createRef({
-//     owner: context.repo.owner,
-//     repo: context.repo.repo,
-//     ref: `refs/heads/${newBranchName}`,
-//     sha: context.sha,
-//   });
+  // Create new branch from base branch
+  const newMergeBranch = await github.rest.git.createRef({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    ref: `refs/heads/${newBranchName}`,
+    sha: context.sha,
+  });
 
-//   // Create pull request to merge
-//   const createdPR = await github.rest.pulls.create({
-//     owner: context.repo.owner,
-//     repo: context.repo.repo,
-//     title: `[BOT] Merge back: ${sourceBranch} into ${targetBranch} 🤖`,
-//     body: `Automatic merging back ${sourceBranch} into ${targetBranch}! @${context.payload.pull_request.user.login} Please verify that the merge is correct.`,
-//     head: newMergeBranch.data.ref,
-//     base: targetBranch,
-//   });
+  // Create pull request to merge
+  const createdPR = await github.rest.pulls.create({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    title: `[BOT] Merge back: ${sourceBranch} into ${targetBranch} 🤖`,
+    body: `Automatic merging back ${sourceBranch} into ${targetBranch}! @${context.payload.pull_request.user.login} Please verify that the merge is correct.`,
+    head: newMergeBranch.data.ref,
+    base: targetBranch,
+  });
 
-//   // Add responsible author as an assignee
-//   await github.rest.issues.addAssignees({
-//     owner: context.repo.owner,
-//     repo: context.repo.repo,
-//     issue_number: createdPR.data.number,
-//     assignees: [context.payload.pull_request.user.login],
-//   });
-// }
+  // Add responsible author as an assignee
+  await github.rest.issues.addAssignees({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: createdPR.data.number,
+    assignees: [context.payload.pull_request.user.login],
+  });
+}
